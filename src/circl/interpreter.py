31819dc80472eee30bccdc312c33c81cd6cfe74d
Laura <lauraprograms@proton.me>
2026-05-11 20:47:18 +0200

    Changes moved to another branch

--- src/circl/interpreter.py
+++ src/circl/interpreter.py
@@ -31,9 +31,10 @@ def execute(executing_circl) -> Any | None:
             if main_program.number_of_counters() == 0:
                 return executing_circl.stdout_copy
             break # Change this later to return the return-value instead
-        current_step = main_program.get_counter()
-        command = executing_circl[current_step]
         try:
+            current_step = main_program.get_counter()
+            command = executing_circl[current_step]
+
             if isinstance(command, Circl) or command not in instruction_set.keys():
                 executing_circl.append(command)
             else:
@@ -44,27 +45,10 @@ def execute(executing_circl) -> Any | None:
                     instruction.operation(executing_circl, execute)
                 else:
                     instruction.operation(executing_circl)
-        
+
         except Exception as e:
-            if isinstance(command, Circl) and command.source_info is not None:
-                line_info: str
-                if command.source_info.from_line == command.source_info.to_line:
-                    line_info = f"line {command.source_info.from_line}"
-                else:
-                    line_info = f"lines {command.source_info.from_line} to {command.source_info.to_line}"
-
-                position_info: str
-                if command.source_info.from_position == command.source_info.to_position:
-                    position_info = f"at position {command.source_info.from_position}"
-                else:
-                    position_info = f"from {command.source_info.from_position} to {command.source_info.to_position}"
-
-                print(f"Exception in {command.source_info.file} on {line_info} {position_info}: {e}")
-            else:
-                print(f"Exception: {e}")
-            if main_program.verbose_exceptions:
-                traceback.print_stack()
-            print("Appending current exception to current circl")
+            traceback.print_exception(e)
+            print("An Exception, ", e, " occured. Pushing to stack and continuing")
             executing_circl.append(str(e))
 
         main_program.increment_counter()
